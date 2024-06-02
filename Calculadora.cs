c985e83fd468fc58fbd5837ae603698e912b9210
Benja <benjaminvilla409@gmail.com>
2024-06-02 01:39:27 -0300

    Cambios en calculadora realizados.

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,23 +1,59 @@
 namespace CalculadoraNamespace;
+public enum TipoOperacion{
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Limpiar
+}
+public class Operacion{
+    private double resultadoAnterior;
+    private double nuevoValor;
+    private TipoOperacion operacion;
+
+    public Operacion(double resultadoAnterior, double nuevoValor, TipoOperacion operacion){
+        this.resultadoAnterior = resultadoAnterior;
+        this.nuevoValor = nuevoValor;
+        this.operacion = operacion;
+    }
+
+    public double Resultado { get => resultadoAnterior;}
+    public double NuevoValor { get => nuevoValor;}
+}
 class Calculadora{
     private double dato;
-    public double Resultado{
-        set{}
-        get{ return dato;}
-    }
+    private List<Operacion> historial = new List<Operacion>();
+    public double Resultado{get => dato;}
+    public List<Operacion> Historial { get => historial; }
+
     public void Sumar(double termino){
-        dato+=termino;
+        double nuevoValor = dato + termino;
+        Operacion op = new(dato,nuevoValor,TipoOperacion.Suma);
+        historial.Add(op);
+        dato = nuevoValor;
     }
     public void Restar(double termino){
-        dato-=termino;
+        double nuevoValor = dato - termino;
+        Operacion op = new(dato,nuevoValor,TipoOperacion.Resta);
+        historial.Add(op);
+        dato = nuevoValor;
     }
     public void Multiplicar(double termino){
-        dato*=termino;
+        double nuevoValor = dato*termino;
+        Operacion op = new(dato,nuevoValor,TipoOperacion.Multiplicacion);
+        historial.Add(op);
+        dato = nuevoValor;
     }
     public void Dividir(double termino){
-        dato/=termino;
+        double nuevoValor = dato / termino;
+        Operacion op = new(dato,nuevoValor,TipoOperacion.Division);
+        historial.Add(op);
+        dato = nuevoValor;
     }
     public void Limpiar(){
-        dato=0;
+        Operacion op = new(dato,0,TipoOperacion.Limpiar);
+        historial.Add(op);
+        dato = 0;
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+using CalculadoraNamespace;
+Calculadora calculadora = new Calculadora();
+//calculadora.Sumar(15.0d);
+//Console.WriteLine(calculadora._dato);
+int opciones;
+string? entrada;
+bool esNumeroValido;
+do{
+    Console.WriteLine("-- Seleccione una de las operaciones --");
+    Console.WriteLine("1 - Sumar");
+    Console.WriteLine("2 - Restar");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    Console.WriteLine("5 - Limpiar");
+    Console.WriteLine("6 - Salir");
+
+    entrada = Console.ReadLine();
+    esNumeroValido = int.TryParse(entrada,out opciones);
+
+    if(!esNumeroValido || opciones < 1 || opciones > 6)
+        Console.WriteLine("Ingrese una entrada válida.");
+    else if(opciones == 5){
+        calculadora.Limpiar();
+    }else if(opciones != 6){
+        //Tomamos un parametro
+        double termino;
+        Console.WriteLine("Ingrese un numero: ");
+        bool esTerminoValido = double.TryParse(Console.ReadLine(),out termino);
+        if(esTerminoValido){
+            switch(opciones){
+                case 1:
+                    calculadora.Sumar(termino);
+                    break;
+                case 2:
+                    calculadora.Restar(termino);
+                    break;
+                case 3:
+                    calculadora.Multiplicar(termino);
+                    break;
+                case 4:
+                    calculadora.Dividir(termino);
+                    break;
+            }
+            Console.WriteLine($"El resultado es: {calculadora.Resultado}");
+        }else{
+            Console.WriteLine("El termino ingresado no es válido");
+        }
+    }
+}while(opciones != 6);
