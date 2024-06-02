d9545dbedd77426785308a50ecfec43a71486c2c
Benja <benjaminvilla409@gmail.com>
2024-06-01 23:17:14 -0300

    Proyecto inicializado para comenzar con el ejercicio 2

---
+++ Calculadora.cs
@@ -0,0 +1,23 @@
+namespace CalculadoraNamespace;
+class Calculadora{
+    private double dato;
+    public double Resultado{
+        set{}
+        get{ return dato;}
+    }
+    public void Sumar(double termino){
+        dato+=termino;
+    }
+    public void Restar(double termino){
+        dato-=termino;
+    }
+    public void Multiplicar(double termino){
+        dato*=termino;
+    }
+    public void Dividir(double termino){
+        dato/=termino;
+    }
+    public void Limpiar(){
+        dato=0;
+    }
+}
